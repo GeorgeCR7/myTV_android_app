f2e70e77ed7b5dc93c904dbef1bb8bc180092fa4
GeorgeCR7 <gasdextercr7@yahoo.com>
2023-09-11 19:45:32 +0300

    Finish main actv, add profile, tv, list actvs.

---
+++ app/src/main/java/com/example/mytv_android_app/activities/TVShowActivity.java
@@ -0,0 +1,16 @@
+package com.example.mytv_android_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mytv_android_app.R;
+
+public class TVShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tvshow);
+    }
+}
